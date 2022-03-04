db27d5484e9b1fb7a572c48653a21ba06a3685ff
chi <chi.le@codeenginestudio.c>
2022-03-04 17:19:32 +0700

    R2 - Admin monitor portlet UI & renderer commands

---
+++ Registration-web/src/main/java/com/amf/web/registration/portlet/AdminMonitorPortlet.java
@@ -0,0 +1,31 @@
+package com.amf.web.registration.portlet;
+
+import com.amf.web.registration.constants.RegistrationPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author ces-user
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.Registration",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=AdminMonitor",
+		"javax.portlet.init-param.template-path=/monitor/",
+		"javax.portlet.init-param.view-template=/monitor/view.jsp",
+		"javax.portlet.name=" + RegistrationPortletKeys.ADMIN_MONITOR,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class AdminMonitorPortlet extends MVCPortlet {
+	
+}
