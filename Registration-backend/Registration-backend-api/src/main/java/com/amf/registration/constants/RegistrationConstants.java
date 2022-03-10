ae4c12f0191af5e3da2a0a18b8fcb04adf7bac1c
chi <chi.le@codeenginestudio.c>
2022-03-10 17:35:02 +0700

    R3 - Permission for search with TEC's role

--- Registration-backend/Registration-backend-api/src/main/java/com/amf/registration/constants/RegistrationConstants.java
+++ Registration-backend/Registration-backend-api/src/main/java/com/amf/registration/constants/RegistrationConstants.java
@@ -3,5 +3,6 @@ package com.amf.registration.constants;
 public class RegistrationConstants {
 	public static final String RESOURCE_NAME = "com.amf.registration.model";
 	public static final String RESOURCE_MODEL = "com.amf.registration.model.RegistrationLog";
-
+	public static final String RESOURCE_USER_MODEL_NAME = "com.liferay.portal.kernel.model.User";
+	
 }
